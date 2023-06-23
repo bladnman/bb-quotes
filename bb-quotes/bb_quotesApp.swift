c4be1ed4a8f3eb96809029595e76967c0c81fea8
Matt Maher <matt.maher@sony.com>
2023-06-23 11:49:31 -0700

    Initial Commit

---
+++ bb-quotes/bb_quotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  bb_quotesApp.swift
+//  bb-quotes
+//
+//  Created by Matt Maher on 6/23/23.
+//
+
+import SwiftUI
+
+@main
+struct bb_quotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
